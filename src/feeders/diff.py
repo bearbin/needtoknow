01388c45e6fd050b08e44d11264c76ba53c0b15a
Alexander Lyons Harkness <me@bearbin.net>
2019-04-22 12:01:00 +0100

    Show links and changes thereof in text diffs
    Fixes #1

--- src/feeders/diff.py
+++ src/feeders/diff.py
@@ -1,5 +1,6 @@
 from . import diffcommon, base
 import bs4, difflib
+from urllib.parse import urljoin
 
 class Feeder(base.Feeder):
     def __iter__(self):
@@ -13,7 +14,14 @@ class Feeder(base.Feeder):
                 old = []
                 oldurl = '/dev/null'
             try:
-                new = bs4.BeautifulSoup(base.download(url).strip(), 'html.parser').get_text().splitlines()
+                soup = bs4.BeautifulSoup(base.download(url).strip(), 'html.parser')
+                base_elem = soup.find("base")
+                base_url = url if base_elem is None else base_elem["href"]
+                for a in soup.find_all("a"):
+                    if a["href"] is None: continue
+                    abs_href = urljoin(base_url, a["href"])
+                    a.append(" (" + abs_href + ")")
+                new = soup.get_text().splitlines()
             except Exception as e:
                 yield Exception('Error while loading %(url)s: %(err)s' % {
                     'url':url,
